60757d70541ec1faa1d197b2f1c4d3a29421cb85
Joefhanie <joefhanie@gmail.com>
2025-03-31 12:44:20 +0800

    Add project files.

---
+++ Data/FinanceAppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Expense> Expenses { get; set; } = null!;
+    }
+    {
+
+    }
+}
